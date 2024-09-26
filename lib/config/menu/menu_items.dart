ebf8e93496147284035686e5280a49b836272048
Gabriel <gabrielbaez023@gmail.com>
2024-09-25 20:25:30 -0400

    modificamos estilos de appbar

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+class MenuItems {
+
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+
+    required this.title, 
+    required this.subTitle, 
+    required this.link, 
+    required this.icon});
+
+}
+
+const appMenuItems = <MenuItems>[
+  
+  MenuItems(
+    title: 'Botones',
+    subTitle: 'Varios Botones en flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+  ),
+
+   
+  MenuItems(
+    title: 'Tarjetas',
+    subTitle: 'Un contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card
+  )
+  
+  
+];
