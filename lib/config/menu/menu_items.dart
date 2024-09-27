d1a53279ae4c415c47218557162c1ab88541dd5f
Gabriel <gabrielbaez023@gmail.com>
2024-09-26 20:35:11 -0400

    rutas basica a no usar

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -28,7 +28,7 @@ const appMenuItems = <MenuItems>[
   MenuItems(
     title: 'Tarjetas',
     subTitle: 'Un contenedor estilizado',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card
   )
   
